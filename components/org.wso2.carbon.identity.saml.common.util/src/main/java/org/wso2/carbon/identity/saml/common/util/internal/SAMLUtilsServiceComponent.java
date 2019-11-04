6e007c9cdd21744c769e2b1bd745958e42a68a71
deshankoswatte <dehami.deshan@gmail.com>
2019-11-04 17:28:47 +0530

    Exposed the component as an OSGi service

---
+++ components/org.wso2.carbon.identity.saml.common.util/src/main/java/org/wso2/carbon/identity/saml/common/util/internal/SAMLUtilsServiceComponent.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.carbon.identity.saml.common.util.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.wso2.carbon.identity.saml.common.util.SAMLInitializer;
+
+@Component(name = "org.wso2.carbon.identity.saml.common.util.internal.SAMLUtilsServiceComponent",
+        immediate = true)
+public class SAMLUtilsServiceComponent {
+
+    private static final Log log = LogFactory.getLog(SAMLUtilsServiceComponent.class);
+
+    @Activate
+    protected void activate(BundleContext bundleContext) {
+
+        bundleContext.registerService(SAMLInitializer.class, new SAMLInitializer(), null);
+        if (log.isDebugEnabled()) {
+            log.debug("SAML common utils bundle is activated");
+        }
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext context) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("SAML common utils bundle is de-activated");
+        }
+    }
+
+}
